a09a65d31c465748126531d31a4b61b085e1396c
PabloDinella <pablo.dinella@gmail.com>
2024-11-19 23:39:56 -0300

    initial

---
+++ src/App.tsx
@@ -0,0 +1,28 @@
+import { Button } from "./components/ui/button";
+import {
+  SidebarInset,
+  SidebarProvider,
+  SidebarTrigger,
+} from "./components/ui/sidebar";
+import { AppSidebar } from "./components/app-sidebar";
+import { ThemeProvider } from "./components/theme-provider";
+import { ModeToggle } from "./components/mode-toggle";
+
+function App() {
+  return (
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <SidebarProvider>
+        <AppSidebar />
+        <SidebarInset>
+          <header className="flex h-16 shrink-0 items-center gap-2 border-b px-4">
+            <SidebarTrigger />
+            <ModeToggle />
+            <Button variant="outline">hi</Button>
+          </header>
+        </SidebarInset>
+      </SidebarProvider>
+    </ThemeProvider>
+  );
+}
+
+export default App;
